30ae3115e13c09bdf0667af96c75c63e7d053c2d
Socialforger <socialforger@gmail.com>
2026-01-16 06:22:22 +0100

    Update woosolid.php

--- woosolid.php
+++ woosolid.php
@@ -1,27 +1,29 @@
 <?php
 /**
  * Plugin Name: WooSolid
- * Description: Plugin di configurazione per Woocommerce e WP Crowdfunding Pro.
+ * Description: Plugin per WooCommerce.
  * Author: Socialforge
  * Version: 1.0.0
  * Text Domain: woosolid
  * Domain Path: /languages
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
 define( 'WOOSOLID_PATH', plugin_dir_path( __FILE__ ) );
-define( 'WOOSOLID_URL', plugin_dir_url( __FILE__ ) );
+define( 'WOOSOLID_URL',  plugin_dir_url( __FILE__ ) );
+define( 'WOOSOLID_VERSION', '1.0.0' );
 
-function woosolid_load_textdomain() {
+add_action( 'plugins_loaded', function() {
+
     load_plugin_textdomain(
         'woosolid',
         false,
         dirname( plugin_basename( __FILE__ ) ) . '/languages/'
     );
-}
-add_action( 'plugins_loaded', 'woosolid_load_textdomain' );
 
-require_once WOOSOLID_PATH . 'includes/class-woosolid-bootstrap.php';
-
-add_action( 'plugins_loaded', [ 'WooSolid_Bootstrap', 'init' ] );
+    require_once WOOSOLID_PATH . 'includes/class-woosolid-bootstrap.php';
+    WooSolid_Bootstrap::init();
+});
