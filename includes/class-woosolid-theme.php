f98f90d6fffca5c0b700a055bdecf6c1afdfde25
Socialforger <socialforger@gmail.com>
2026-01-16 04:25:10 +0100

    Create class-woosolid-theme.php

---
+++ includes/class-woosolid-theme.php
@@ -0,0 +1,10 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+class WooSolid_Theme {
+
+    public static function init() {
+        // Placeholder per integrazioni tema (Blocksy, ecc.)
+    }
+}
