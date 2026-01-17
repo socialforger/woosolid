20aebbf02dcf044996f1503dac18440b6ac04a15
Socialforger <socialforger@gmail.com>
2026-01-17 18:18:51 +0100

    Update woosolid.php

--- woosolid.php
+++ woosolid.php
@@ -18,9 +18,37 @@ require_once WOOSOLID_PATH . 'includes/helpers/class-woosolid-utils.php';
 require_once WOOSOLID_PATH . 'includes/helpers/class-woosolid-logger.php';
 
 /**
- * Bootstrap
+ * Core Bootstrap
  */
 require_once WOOSOLID_PATH . 'includes/class-woosolid-bootstrap.php';
 
+/**
+ * WooSolid Modules
+ */
+require_once WOOSOLID_PATH . 'includes/class-woosolid-wizard.php';
+require_once WOOSOLID_PATH . 'includes/class-woosolid-registration.php';
+require_once WOOSOLID_PATH . 'includes/class-woosolid-account.php';
+
+/**
+ * Attivazione plugin
+ */
 register_activation_hook( __FILE__, [ 'WooSolid_Bootstrap', 'activate' ] );
-add_action( 'plugins_loaded', [ 'WooSolid_Bootstrap', 'init' ] );
+
+/**
+ * Inizializzazione plugin
+ */
+add_action( 'plugins_loaded', function() {
+
+    // Core
+    WooSolid_Bootstrap::init();
+
+    // Wizard (pagina nascosta + run setup)
+    WooSolid_Wizard::init();
+
+    // Registrazione avanzata (vecchio modello WooSolid)
+    // NOTA: nel file class-woosolid-registration.php NON deve esserci un altro ::init()
+    WooSolid_Registration::init();
+
+    // Account (colonne ordini + dettagli consegna)
+    WooSolid_Account::init();
+});
