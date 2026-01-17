5b07e20c9a8c2dea3e909d82d15ae7791a083b47
Socialforger <socialforger@gmail.com>
2026-01-17 11:54:29 +0100

    Update class-woosolid-settings.php

--- includes/class-woosolid-settings.php
+++ includes/class-woosolid-settings.php
@@ -1,16 +1,16 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
 class WooSolid_Settings {
 
-    const OPTION_NAME = 'woosolid_settings';
-
     public static function init() {
-        add_action( 'admin_menu', [ __CLASS__, 'menu' ] );
+        add_action( 'admin_menu', [ __CLASS__, 'add_menu_page' ] );
         add_action( 'admin_init', [ __CLASS__, 'register_settings' ] );
     }
 
-    public static function menu() {
+    public static function add_menu_page() {
         add_menu_page(
             __( 'WooSolid', 'woosolid' ),
             'WooSolid',
@@ -23,7 +23,11 @@ class WooSolid_Settings {
     }
 
     public static function register_settings() {
-        register_setting( 'woosolid_settings_group', self::OPTION_NAME );
+        register_setting( 'woosolid_settings', 'woosolid_enable_shipping' );
+        register_setting( 'woosolid_settings', 'woosolid_enable_pickup' );
+        register_setting( 'woosolid_settings', 'woosolid_ets_email' );
+        register_setting( 'woosolid_settings', 'woosolid_ets_name' );
+        register_setting( 'woosolid_settings', 'woosolid_ets_cf' );
 
         add_settings_section(
             'woosolid_main',
@@ -32,42 +36,86 @@ class WooSolid_Settings {
             'woosolid-settings'
         );
 
-        self::add_field( 'enable_shipping', __( 'Abilita spedizione', 'woosolid' ) );
-        self::add_field( 'enable_pickup_points', __( 'Abilita punti di ritiro', 'woosolid' ) );
-        self::add_field( 'woosolid_campaign_id', __( 'ID campagna Charitable', 'woosolid' ) );
-        self::add_field( 'woosolid_ets_email', __( 'Email ETS per ordini e rettifiche', 'woosolid' ) );
-    }
-
-    private static function add_field( $id, $label ) {
         add_settings_field(
-            $id,
-            $label,
-            [ __CLASS__, 'render_field' ],
+            'woosolid_enable_shipping',
+            __( 'Abilita spedizione', 'woosolid' ),
+            [ __CLASS__, 'field_enable_shipping' ],
             'woosolid-settings',
-            'woosolid_main',
-            [ 'id' => $id ]
+            'woosolid_main'
+        );
+
+        add_settings_field(
+            'woosolid_enable_pickup',
+            __( 'Abilita punti di ritiro', 'woosolid' ),
+            [ __CLASS__, 'field_enable_pickup' ],
+            'woosolid-settings',
+            'woosolid_main'
+        );
+
+        add_settings_field(
+            'woosolid_ets_email',
+            __( 'Email ETS per ordini e rettifiche', 'woosolid' ),
+            [ __CLASS__, 'field_ets_email' ],
+            'woosolid-settings',
+            'woosolid_main'
+        );
+
+        add_settings_field(
+            'woosolid_ets_name',
+            __( 'Denominazione ETS', 'woosolid' ),
+            [ __CLASS__, 'field_ets_name' ],
+            'woosolid-settings',
+            'woosolid_main'
+        );
+
+        add_settings_field(
+            'woosolid_ets_cf',
+            __( 'Codice Fiscale ETS', 'woosolid' ),
+            [ __CLASS__, 'field_ets_cf' ],
+            'woosolid-settings',
+            'woosolid_main'
         );
     }
 
-    public static function render_field( $args ) {
-        $settings = self::get_settings();
-        $id = $args['id'];
-        $value = isset( $settings[ $id ] ) ? $settings[ $id ] : '';
+    public static function field_enable_shipping() {
+        $value = get_option( 'woosolid_enable_shipping', 'yes' );
+        ?>
+        <label>
+            <input type="checkbox" name="woosolid_enable_shipping" value="yes" <?php checked( $value, 'yes' ); ?> />
+            <?php esc_html_e( 'Abilita', 'woosolid' ); ?>
+        </label>
+        <?php
+    }
 
-        if ( in_array( $id, [ 'enable_shipping', 'enable_pickup_points' ], true ) ) {
-            ?>
-            <label>
-                <input type="checkbox" name="<?php echo esc_attr( self::OPTION_NAME . '[' . $id . ']' ); ?>" value="1" <?php checked( $value, 1 ); ?> />
-                <?php esc_html_e( 'Abilita', 'woosolid' ); ?>
-            </label>
-            <?php
-        } else {
-            ?>
-            <input type="text" class="regular-text"
-                   name="<?php echo esc_attr( self::OPTION_NAME . '[' . $id . ']' ); ?>"
-                   value="<?php echo esc_attr( $value ); ?>" />
-            <?php
-        }
+    public static function field_enable_pickup() {
+        $value = get_option( 'woosolid_enable_pickup', 'yes' );
+        ?>
+        <label>
+            <input type="checkbox" name="woosolid_enable_pickup" value="yes" <?php checked( $value, 'yes' ); ?> />
+            <?php esc_html_e( 'Abilita', 'woosolid' ); ?>
+        </label>
+        <?php
+    }
+
+    public static function field_ets_email() {
+        $value = get_option( 'woosolid_ets_email', '' );
+        ?>
+        <input type="email" name="woosolid_ets_email" value="<?php echo esc_attr( $value ); ?>" class="regular-text" />
+        <?php
+    }
+
+    public static function field_ets_name() {
+        $value = get_option( 'woosolid_ets_name', '' );
+        ?>
+        <input type="text" name="woosolid_ets_name" value="<?php echo esc_attr( $value ); ?>" class="regular-text" />
+        <?php
+    }
+
+    public static function field_ets_cf() {
+        $value = get_option( 'woosolid_ets_cf', '' );
+        ?>
+        <input type="text" name="woosolid_ets_cf" value="<?php echo esc_attr( $value ); ?>" class="regular-text" />
+        <?php
     }
 
     public static function render_page() {
@@ -76,23 +124,12 @@ class WooSolid_Settings {
             <h1><?php esc_html_e( 'Impostazioni WooSolid', 'woosolid' ); ?></h1>
             <form method="post" action="options.php">
                 <?php
-                settings_fields( 'woosolid_settings_group' );
+                settings_fields( 'woosolid_settings' );
                 do_settings_sections( 'woosolid-settings' );
                 submit_button();
                 ?>
             </form>
         </div>
         <?php
-    }
-
-    public static function get_settings() {
-        $saved = get_option( self::OPTION_NAME, [] );
-
-        return wp_parse_args( $saved, [
-            'enable_shipping'      => 1,
-            'enable_pickup_points' => 1,
-            'woosolid_campaign_id' => '',
-            'woosolid_ets_email'   => '',
-        ] );
     }
 }
