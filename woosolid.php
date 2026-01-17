ac798f3e13b98aa0e4655e1d9a202d2d51fd75d1
Socialforger <socialforger@gmail.com>
2026-01-17 11:52:04 +0100

    Update woosolid.php

--- woosolid.php
+++ woosolid.php
@@ -1,11 +1,9 @@
 <?php
 /**
  * Plugin Name: WooSolid
- * Description: Plugin per WooCommerce.
+ * Description: Plugin per Woocommerce
  * Author: Socialforge
  * Version: 1.0.0
- * Text Domain: woosolid
- * Domain Path: /languages
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,17 +11,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 define( 'WOOSOLID_PATH', plugin_dir_path( __FILE__ ) );
-define( 'WOOSOLID_URL',  plugin_dir_url( __FILE__ ) );
-define( 'WOOSOLID_VERSION', '1.0.0' );
+define( 'WOOSOLID_URL', plugin_dir_url( __FILE__ ) );
 
-add_action( 'plugins_loaded', function() {
+require_once WOOSOLID_PATH . 'includes/helpers/class-woosolid-utils.php';
+require_once WOOSOLID_PATH . 'includes/helpers/class-woosolid-logger.php';
+require_once WOOSOLID_PATH . 'includes/class-woosolid-bootstrap.php';
 
-    load_plugin_textdomain(
-        'woosolid',
-        false,
-        dirname( plugin_basename( __FILE__ ) ) . '/languages/'
-    );
+register_activation_hook( __FILE__, [ 'WooSolid_Bootstrap', 'activate' ] );
 
-    require_once WOOSOLID_PATH . 'includes/class-woosolid-bootstrap.php';
-    WooSolid_Bootstrap::init();
-});
+add_action( 'plugins_loaded', [ 'WooSolid_Bootstrap', 'init' ] );
