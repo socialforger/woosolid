5b916f48dda400fc391c0e33834e4c4e446a4f63
Socialforger <socialforger@gmail.com>
2026-01-17 11:52:45 +0100

    Update class-woosolid-utils.php

--- includes/helpers/class-woosolid-utils.php
+++ includes/helpers/class-woosolid-utils.php
@@ -1,9 +1,20 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
 class WooSolid_Utils {
 
-    public static function format_money( $amount ) {
-        return wc_price( $amount );
+    public static function is_woocommerce_active() {
+        return class_exists( 'WooCommerce' );
+    }
+
+    public static function is_charitable_active() {
+        return class_exists( 'Charitable' );
+    }
+
+    public static function get_option_bool( $key, $default = false ) {
+        $value = get_option( $key, $default ? 'yes' : 'no' );
+        return 'yes' === $value;
     }
 }
