f50acc099c337b2ca6b5c83be82ac3346de9825b
Socialforger <socialforger@gmail.com>
2026-01-17 11:53:20 +0100

    Update class-woosolid-logger.php

--- includes/helpers/class-woosolid-logger.php
+++ includes/helpers/class-woosolid-logger.php
@@ -1,11 +1,13 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
 
 class WooSolid_Logger {
 
-    public static function log( $message, $context = [] ) {
+    public static function log( $message ) {
         if ( defined( 'WP_DEBUG' ) && WP_DEBUG ) {
-            error_log( '[WooSolid] ' . $message . ( $context ? ' ' . wp_json_encode( $context ) : '' ) );
+            error_log( '[WooSolid] ' . print_r( $message, true ) );
         }
     }
 }
